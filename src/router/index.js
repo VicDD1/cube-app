7f9657cf8243f4cc6ecbc47da78d3d6b45ea211c
Victor Duchesne <victor.ducehsne-delapierre@etu.univ-smb.fr>
2026-03-11 09:06:14 +0100

    mise en place des dossiers

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/produits',
+            name: 'products',
+          // Chargement différé (Lazy-loading) pour plus de performance
+            component: () => import('../views/ProductsView.vue')
+        }
+    ]
+})
+
+export default router
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Ma Page Produits</h1>
+        <p>Bientôt les données de l'API Azure ici !</p>
+    </div>
+</template>
